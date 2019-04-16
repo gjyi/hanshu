8ed5c726ddb88c39d0952d572781895a698a4901
gjyi <43664031+gjyi@users.noreply.github.com>
2019-04-16 11:19:46 +0800

    25

---
+++ div/a.js
@@ -0,0 +1,6 @@
+  function init() {
+    var divSign = document.getElementById("o")
+    divSign.innerHTML = ''
+    divSign.setAttribute('style','height:300px ; width:400px ; background-color: black ; margin-left:300px ; margin-top:100px')
+  }
+  window.onload = init;
